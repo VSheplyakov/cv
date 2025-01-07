7bcabb78ec8e49b26938a31e4ccd26376e4f4afa
VSheplyakov <valeriy.sheplyakov@gmail.com>
2025-01-07 18:01:13 +0200

    new language switcher

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-function changeLanguage(lang) {
-  i18next.changeLanguage(lang, updateContent);
-}
-
 function updateContent() {
-  document.querySelectorAll("[data-i18n]").forEach(function (element) {
+  document.querySelectorAll("[data-i18n]").forEach((element) => {
     const key = element.getAttribute("data-i18n");
     element.textContent = i18next.t(key);
   });
 }
+
+function addLanguageLinksListeners() {
+  document.querySelectorAll(".language-option").forEach((langLink) => {
+    langLink.addEventListener("click", (event) => {
+      event.preventDefault();
+      const selectedLang = langLink.dataset.lang;
+
+      i18next.changeLanguage(selectedLang, () => {
+        updateContent();
+        document
+          .querySelectorAll(".language-option")
+          .forEach((l) => l.classList.remove("language-option--active"));
+
+        langLink.classList.add("language-option--active");
+      });
+    });
+  });
+}
+document.addEventListener("DOMContentLoaded", () => {
+  addLanguageLinksListeners();
+  updateContent(); 
+});
